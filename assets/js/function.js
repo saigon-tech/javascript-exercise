72fc6cfe81dc1debde8aba91d8a862ac46799e55
PhucDoVan <dvphuc.sgt@gmail.com>
2020-04-14 14:04:28 +0700

    #9 update validate and datepicker plugin

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,35 +1,57 @@
 $(function () {
-    $('.logbtn').click(function (e) {
-        e.preventDefault();
-        var text;
-        var lg_id = $('#lg_id').val();
-        var password = $('#password').val();
-        var name = $('#name');
-        var birthday = $('#birthday').val();
-        var email = $('#email').val();
-        var bien = new RegExp('\s');
-        switch (true) {
-            case (lg_id == "" || bien.test(lg_id)):
-                $('.error_id').html("Login ID can not be empty or do not contain spaces");
-                break;
-            case (password == "" || password.length <= 8 || bien.test(password)):
-                $('.error_pw').html("Password Can not be empty or password at least 8 characters or do not contain spaces");
-                break;
-            case (name == "" || name.length > 50):
-                $('.error_name').html("Name can't exceed 50 characters");
-                break;
-            case(email == "" || bien.test(email)):
-                $('.error_email').html("Email can not be empty or do not contain spaces");
-                break;
-            default:
-                alert("All Pass");
+    $("#loginForm").validate({
+        rules:{
+            username:{
+                required:true,
+                pattern:'^[a-zA-Z]+$'
+            },
+            password:{
+                required:true,
+                minlength:8,
+                pattern: '^\\S*$'
+            },
+            fullname:{
+                required: true,
+                maxlength:50
+            },
+            birthday:"required",
+            email:{
+                required:true,
+                email:true
+            }
+        },
+        messages:{
+            username:{
+                required:"Vui lòng nhập username của bạn !",
+                pattern: "Username là alphabet không chứa số và khoảng trống !"
+            },
+            password:{
+                required:"Vui lòng nhập password của bạn !",
+                minlength: "Password của bạn ít hơn 8 ký tự,mời nhập lại !",
+                pattern: "Password không chứa khoảng trống !"
+            },
+            fullname:{
+                required:"Vui lòng nhập name của bạn !",
+                maxlength: "Name không được quá 50 ký tự !"
+            },
+            birthday: "Vui lòng nhâp ngày sinh của bạn !",
+            email:{
+                required:"Vui lòng nhập email của bạn !",
+                email:"Email không đúng format (dvphuc.sgt@gmail.com) !"
+            }
         }
     });
-    $("#birthday").on("change", function() {
-        this.setAttribute(
-            "data-date",
-            moment(this.value, "YYYY-MM-DD")
-                .format( this.getAttribute("data-date-format") )
-        )
-    }).trigger("change")
+    $('#birthday').datepicker();
+    $(".logbtn").click(function () {
+        if ($("#loginForm").valid()) {
+            alert("Successfully");
+        }
+    });
+    // $("#birthday").on("change", function() {
+    //     this.setAttribute(
+    //         "data-date",
+    //         moment(this.value, "YYYY-MM-DD")
+    //             .format( this.getAttribute("data-date-format") )
+    //     )
+    // }).trigger("change")
 });
